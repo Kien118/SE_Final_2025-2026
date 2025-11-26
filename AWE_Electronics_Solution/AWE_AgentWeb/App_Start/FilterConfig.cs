424af0710abd850f461f7df9be4f22f746499de2
Kien118 <huynhkien118@gmail.com>
2025-11-27 00:29:40 +0700

    Setup 3-Tier Architecture References

---
+++ AWE_Electronics_Solution/AWE_AgentWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AWE_AgentWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
